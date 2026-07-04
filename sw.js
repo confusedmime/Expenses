5f39317b31cbb434877488644906c13c37d59ae3
confusedmime <confusedmime@gmail.com>
2026-07-04 01:30:24 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bills-v2";
+const CACHE = "bills-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
